df8bcc04143d7a98a58196bf370dce769d947b15
Starlight0798 <244057735@qq.com>
2024-06-05 00:46:02 +0800

    hyper

--- LunarLander(PPO+RNN).py
+++ LunarLander(PPO+RNN).py
@@ -32,11 +32,11 @@ class Config(BasicConfig):
 
 class ActorCritic(BaseRNNModel):
     def __init__(self, cfg):
-        super(ActorCritic, self).__init__(device=cfg.device, hidden_size=32)
-        self.fc_head = MLP([cfg.n_states, 128])
-        self.rnn = MLPRNN(128, 128, batch_first=True)
-        self.actor_fc = MLP([128, cfg.n_actions])
-        self.critic_fc = MLP([128, 16, 1])
+        super(ActorCritic, self).__init__(device=cfg.device, hidden_size=64)
+        self.fc_head = MLP([cfg.n_states, 256])
+        self.rnn = MLPRNN(256, 256, batch_first=True)
+        self.actor_fc = MLP([256, 64, cfg.n_actions])
+        self.critic_fc = MLP([256, 32, 1])
 
     def forward(self, s):
         x = self.fc_head(s)
